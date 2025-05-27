6f21305380faf4ed4975793584e05df8e938e379
thyvon <thyvonny@gmail.com>
2025-05-27 21:55:34 +0700

    upate

--- resources/js/components/reusable/Datatable.vue
+++ resources/js/components/reusable/Datatable.vue
@@ -192,7 +192,7 @@ const initDataTable = () => {
     processing: true,
     serverSide: true,
     dom:
-      "<'row mb-3'<'col-sm-12 col-md-6 d-flex align-items-center justify-content-start'f><'col-sm-12 col-md-6 d-flex align-items-center justify-content-end'B>>" +
+      "<'row mb-3'<'col-sm-12 col-md-6 d-flex align-items-center justify-content-start'l><'col-sm-12 col-md-6 d-flex align-items-center justify-content-end'Bf>>" +
       "<'row'<'col-sm-12'tr>>" +
       "<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>",
     buttons: [
